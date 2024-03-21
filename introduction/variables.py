4db2564e581f213e57f1cb411e5e7e884fb5d163
W45 <73011833+wgs45@users.noreply.github.com>
2024-03-21 22:21:27 +0800

    Add files via upload

---
+++ introduction/variables.py
@@ -0,0 +1,18 @@
+# Different python data types
+
+first_name = 'Alpha'     # str
+last_name = 'Beta'       # str
+country = 'Japan'         # str
+city= 'Tokyo'            # str
+age = 25               # int
+
+print(type('Alpha'))     # str
+print(type(first_name))     # str
+print(type(10))             # int
+print(type(3.14))           # float
+print(type(1 + 1j))         # complex
+print(type(True))           # bool
+print(type([1, 2, 3, 4]))     # list
+print(type({'name':'Alpha','age':25, 'is_married':25}))    # dict
+print(type((1,2)))                                              # tuple
+print(type(zip([1,2],[3,4])))                                   # set 
